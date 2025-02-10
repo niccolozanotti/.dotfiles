b7eb7a41f13525d8b9b6f6997ff3db034f7a3330
Niccolo' Zanotti <niccolo.zanotti@icloud.com>
2025-02-11 00:14:03 +0100

    (tmux/nvim): add tmux configuration
    * add `tmux.conf`
    * add plugin vim-tmux-navigator to neovim

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { silent = true })
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { silent = true })
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { silent = true })
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { silent = true })
+map("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<CR>", { silent = true })
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -70,4 +70,22 @@ return {
             require("configs.mason-lint")
         end,
     },
+    {
+        "christoomey/vim-tmux-navigator",
+        lazy = false,
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        -- keys = {
+        --     { "<C-h>", "<cmd>TmuxNavigateLeft<CR>", desc = "Navigate Left" },
+        --     { "<C-j>", "<cmd>TmuxNavigateDown<CR>", desc = "Navigate Down" },
+        --     { "<C-k>", "<cmd>TmuxNavigateUp<CR>", desc = "Navigate Up" },
+        --     { "<C-l>", "<cmd>TmuxNavigateRight<CR>", desc = "Navigate Right" },
+        --     -- { "<C-\\>", "<cmd>TmuxNavigatePrevious<CR>", desc = "Navigate Previous" },
+        -- },
+    },
 }
