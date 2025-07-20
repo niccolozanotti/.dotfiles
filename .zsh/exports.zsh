31ca1b893419f82c80099148b9ba7005e7350eb9
Niccolò Zanotti <niccolo.zanotti@protonmail.com>
2025-07-20 18:41:44 +0200

    (zsh): update env variables

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -5,12 +5,8 @@ else
   export EDITOR='nvim'
 fi
 
-export XDG_CONFIG_HOME="$HOME/.config"
-export SPACK_ROOT=$HOME/spack
-
 #GPG
 export GPG_TTY=$(tty)
-
 
 ## PYTHON
 export PYTHON3_HOMEBREW="$HOMEBREW_ROOT/bin/python3"
@@ -29,9 +25,9 @@ export PIP3_11="$UV_PYTHON_ROOT/cpython-3.11.11-macos-aarch64-none/bin/pip3"
 export PIP3_12="$UV_PYTHON_ROOT/cpython-3.12.11-macos-aarch64-none/bin/pip3"
 export PIP3_13="$UV_PYTHON_ROOT/cpython-3.13.1-macos-aarch64-none/bin/pip3"
 
-# ICCS-related work
-export ICCS="$HOME/Documents/iccs"
-export FTORCH="$ICCS/FTorch"
+#useful env variables
+export SOFTWARE="$HOME/software"
+export ICCS="$SOFTWARE/iccs/"
 export FTORCH_PREFIX="$ICCS/libs/bin/FTorchbin/lib/cmake/FTorch/"
 export LIBTORCH_PREFIX="$ICCS/libs/libtorch/share/cmake/Torch/"
-export PFUNIT_PREFIX="$ICCS/libs/PFUNIT-4.12/cmake/"
+export PFUNIT_PREFIX="$ICCS/libs/pFUnit/build/installed/PFUNIT-4.12/cmake"
--- .zshrc
+++ .zshrc
@@ -29,13 +29,19 @@ source $HOMEBREW_ROOT/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 source $HOMEBREW_ROOT/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $HOMEBREW_ROOT/share/powerlevel10k/powerlevel10k.zsh-theme
 
+# Cross Desktop Group specification
+# https://specifications.freedesktop.org/basedir-spec/latest/
+export XDG_CONFIG_HOME="$HOME/.config"
+
 # spack.io
+export SPACK_ROOT="$HOME/spack"
 source $SPACK_ROOT/share/spack/setup-env.sh
 # brew.sh
 eval "$(brew shellenv)"
 
 # Julia
 export PATH=$PATH:$HOME/.juliaup/bin
+
 # avoid clobbering for safety; override behavior with >|
 set -o noclobber  
 
